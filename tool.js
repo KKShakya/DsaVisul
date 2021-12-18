87f933bcc7f2e4247c2ba79cb58f86cc30a0d024
KRISHNA Kumar Shakya <Krishnashakya7272@gmail.com>
2021-12-18 11:24:56 +0530

    Add files via upload

--- tool.js
+++ tool.js
@@ -1,1 +1,5 @@
-console.log("hellow World")
+let toolbox = document.getElementById("ToolBox");
+let toolboxele = document.querySelectorAll(".Tools>elements");
+let savefile = document.getElementById("save-B");
+let drawing_area = document.getElementById("draw_main");
+let clear = document.getElementById("clear");
